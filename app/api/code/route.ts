4639d03952b5430696eb889b4b235dff46cc10fd
jj-sallz89 <gitboi56@outlook.com>
2023-08-19 14:00:13 +0100

    code generation complete

---
+++ app/api/code/route.ts
@@ -0,0 +1,48 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { ChatCompletionResponseMessage, Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai =  new OpenAIApi(configuration);
+
+const instructionsMessage: ChatCompletionResponseMessage = {
+    role: "system",
+    content: "you are a code generator. You must answer answer in only markdown code snippets. Use code comments for explantions"
+}
+
+export async function POST(
+    req:Request
+) {
+    try{
+        const {userId} = auth();
+        const body = await req.json();
+        const {messages} = body;
+        
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+          }
+      
+          if (!configuration.apiKey) {
+            return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+          }
+      
+          if (!messages) {
+            return new NextResponse("Messages are required", { status: 400 });
+          }
+          
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [instructionsMessage, ...messages],
+
+          });
+      
+  return NextResponse.json(response.data.choices[0].message);
+    } catch (error)  {
+        console.log("[CODE ERROR]", error);
+        return new NextResponse("Internel error", {status: 500});
+    }
+}
